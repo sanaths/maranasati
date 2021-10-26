f878e20d0064c1e780cc443346c73762d5e5312b
devnoman0 <yabdullahalnoman@gmail.com>
2021-10-26 16:12:25 +0600

    Final update changes

--- background.js
+++ background.js
@@ -1,52 +1,46 @@
 console.log("background running");
 
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log("Installex");
+
+  chrome.storage.local.set({ sleepTime: undefined });
+});
+
 //Append the dragable timer element when a new tab opened and page load/url loaded
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  //Get draggable showing status
+  chrome.storage.local.get("drag_elem_showing", ({ drag_elem_showing }) => {
+    if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+      if (drag_elem_showing === "showing") {
+        chrome.scripting.insertCSS({
+          target: { tabId: tabId },
+          files: ["dragableElem.css"],
+        });
 
-
-        //Get draggable showing status
-       chrome.storage.local.get("drag_elem_showing", ({ drag_elem_showing }) => {
-
-                if(changeInfo.status === "complete" && /^http/.test(tab.url)){
-                    if(drag_elem_showing === "showing"){
-
-
-                                    chrome.scripting.insertCSS({
-                                        target: { tabId: tabId },
-                                        files: ['dragableElem.css'],
-                                    });
-
-                                    chrome.scripting.executeScript({
-                                        target: { tabId: tabId },
-                                        files: ['dragableElem.js'],
-                                    });   
-                            }else{
-
-                                console.log("Showing is off");
-                            }
-
-                }
-
-       })
-})
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          files: ["dragableElem.js"],
+        });
+      } else {
+        console.log("Showing is off");
+      }
+    }
+  });
+});
 
 //Append the dragable timer element when a new tab opened and page load/url loaded
-chrome.tabs.onActivated.addListener(async activeInfo => {
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  if (tab.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ["dragableElem.css"],
+    });
 
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-        if(tab.status === "complete" && /^http/.test(tab.url)){
-        
-                chrome.scripting.insertCSS({
-                    target: { tabId: tab.id },
-                    files: ['dragableElem.css'],
-                });
-
-                chrome.scripting.executeScript({
-                    target: { tabId: tab.id },
-                    files: ['dragableElem.js'],
-                });   
-                    
-        }
-})
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["dragableElem.js"],
+    });
+  }
+});
